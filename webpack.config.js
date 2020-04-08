8f414357ace1074865e1d3030981dfb476bc3a2e
Jair Jaramillo <jair.jaramillo@outlook.com>
2020-04-08 00:23:50 -0500

    Refactor file

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,42 @@
 /* eslint-disable no-unused-vars */
-/* eslint-disable import/no-unresolved */
 /* eslint-disable global-require */
 
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const eslint = {
+  test: /\.(js|jsx)$/i,
+  enforce: 'pre',
+  exclude: /node_modules/,
+  use: 'eslint-loader',
+};
+
+const font = {
+  test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/i,
+  use: [{ loader: 'file-loader', options: { name: '[name].[ext]' } }],
+};
+
+const fontAwesome = {
+  test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/i,
+  use: [{
+    loader: 'file-loader',
+    options: {
+      name: '[name].[ext]',
+      outputPath: '../fonts/',
+      publicPath: '../static/fonts',
+    },
+  }],
+};
+
+const scss = {
+  test: /\.s[ac]ss$/i,
+  use: [
+    { loader: 'style-loader' },
+    { loader: 'css-loader' },
+    { loader: 'sass-loader' },
+    { loader: 'postcss-loader', options: { plugins() { return [require('autoprefixer')]; } } },
+  ],
+};
 
 module.exports = {
   mode: 'development',
@@ -15,71 +47,13 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        enforce: 'pre',
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: 'eslint-loader',
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.(scss)$/,
-        use: [{
-          loader: 'style-loader',
-        }, {
-          loader: 'css-loader',
-        }, {
-          loader: 'postcss-loader',
-          options: {
-            plugins() {
-              return [
-                require('autoprefixer'),
-              ];
-            },
-          },
-        }, {
-          loader: 'sass-loader',
-        }],
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader',
-        ],
-      },
-      {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-            },
-          },
-        ],
-      },
-      {
-        test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]',
-            outputPath: '../fonts/',
-            publicPath: '../static/fonts',
-          },
-        }],
-      },
+      eslint,
+      scss,
+      font,
+      fontAwesome,
     ],
   },
   plugins: [
-    // new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       hash: true,
       title: 'Weatherwave',
